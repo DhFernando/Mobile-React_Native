ec1efa6b792663c7291d2986f5e4bc30ff2b0084
dhfernando <hdilshan.fernando11@gmail.com>
2021-01-21 20:34:22 +0530

    fetching data using axios from jsonplaceholder

--- src/component/ReviewApp/HomeTabs/Tab2.jsx
+++ src/component/ReviewApp/HomeTabs/Tab2.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React , { useState, useEffect } from 'react'
 import { View, Text } from 'react-native'
+import Axios from 'axios'
 
 const Tab2 = () => {
+
+    useEffect(()=>{
+        Axios.get("https://jsonplaceholder.typicode.com/users")
+        .then((res)=>{
+            setUsers(res.data)
+        })
+    },[])
+
+    const [ users , setUsers ] = useState(null)
+
     return (
         <View>
-            <Text>Teb2</Text>
+            <View>{users != null && (<View>{ users.map((user)=>{
+                return(<Text key={ user.id }>{ user.name }</Text>)
+            }) }</View>)}</View>
         </View>
     )
 }
